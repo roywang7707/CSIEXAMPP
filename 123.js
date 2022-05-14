0fe4d68fca1b3b74dbaeb5382fef94fd6c553235
roywang7707 <91453053+roywang7707@users.noreply.github.com>
2022-05-14 15:20:02 +0800

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,47 @@
+function main(){
+    
+
+
+    var svg = d3.select("svg"),
+    margin = 200,
+    width = svg.attr("width") - margin,
+    height = svg.attr("height") - margin;
+	
+	
+    svg.append("text")
+            .attr("transform", "translate(100,0")
+            .attr("x", 50)
+            .attr("y", 50)
+            .attr("font-size", "24px")
+            .text("4/1各位置累計時數圖");
+            
+	var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+        yScale = d3.scaleLinear().range([height, 0]);
+
+    var g = svg.append("g").attr("transform", "translate("+100+","+100+")");
+
+    d3.csv("C:/xampp/htdocs/CSIE/123.csv",function(data){
+
+        xScale.domain(data.map(function(d){ return d.location;}));
+        yScale.domain([0, d3.max(data, function(d){return d.date;})]);
+
+        g.append("g")
+            .attr('transform', 'translate(0,' + height+'0)')
+            .call(d3.axisBottom(xScale))
+
+        g.append('g').call(d3.axisLeft(yScale).tickFormat(function(d){
+            return "$" + d;
+        }).ticks(10));
+
+    g.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d){return xScale(d.location);})
+            .attr("y", function(d){return yScale(d.date);})
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d){return height - yScale(d.value);});
+
+
+    });
+}
